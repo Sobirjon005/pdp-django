3d8e5835d505556f49e2d6431839adc685630cb7
Sobirjon005 <sobirjon0305@gmail.com>
2024-11-09 15:04:05 +0500

    tasks

--- 02-django-modul/Netflix/Config/settings.py
+++ 02-django-modul/Netflix/Config/settings.py
@@ -82,23 +82,23 @@ WSGI_APPLICATION = 'Config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "netflix",
-        "USER": "postgres",
-        "PASSWORD": "onamotam",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "netflix",
+#         "USER": "postgres",
+#         "PASSWORD": "onamotam",
+#         "HOST": "127.0.0.1",
+#         "PORT": "5432",
+#     }
+# }
 
 
 # Password validation
